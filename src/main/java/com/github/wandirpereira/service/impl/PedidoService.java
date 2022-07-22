58988729b37df3e585dfb0948ff323aeadcde0a8
Wandir Pereira Filho <wandir@gmail.com>
2022-07-21 23:11:54 -0300

    Criando arquivos Pedido

--- src/main/java/com/github/wandirpereira/service/impl/PedidoService.java
+++ src/main/java/com/github/wandirpereira/service/impl/PedidoService.java
@@ -4,9 +4,11 @@ import com.github.wandirpereira.domain.entity.Pedido;
 import com.github.wandirpereira.domain.enums.StatusPedido;
 import com.github.wandirpereira.rest.dto.PedidoDTO;
 import com.github.wandirpereira.service.IPedidoService;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class PedidoService implements IPedidoService {
     @Override
     public Pedido salvar(PedidoDTO dto) {
