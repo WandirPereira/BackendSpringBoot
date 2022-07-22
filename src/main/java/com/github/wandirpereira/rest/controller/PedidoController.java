1c0ddc4519f5f330e8055d1231f90efb4408d6e4
Wandir Pereira Filho <wandir.filho@csn.com.br>
2022-07-22 14:10:47 -0300

    Atualizando gitignore - criando PedidosService

--- src/main/java/com/github/wandirpereira/rest/controller/PedidoController.java
+++ src/main/java/com/github/wandirpereira/rest/controller/PedidoController.java
@@ -1,9 +1,10 @@
 package com.github.wandirpereira.rest.controller;
 
 
+import com.github.wandirpereira.domain.entity.Pedido;
+import com.github.wandirpereira.rest.dto.PedidoDTO;
 import com.github.wandirpereira.service.impl.PedidoService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/pedidos")
@@ -15,5 +16,11 @@ public class PedidoController {
         this.pedidoService = pedidoService;
     }
 
+    @PostMapping
+    @ResponseStatus
+    public  Integer save (@RequestBody PedidoDTO dto){
+        Pedido pedido = pedidoService.salvar(dto);
+        return pedido.getId();
+    }
 
 }
--- src/main/java/com/github/wandirpereira/service/impl/PedidoService.java
+++ src/main/java/com/github/wandirpereira/service/impl/PedidoService.java
@@ -1,18 +1,78 @@
 package com.github.wandirpereira.service.impl;
 
+import com.github.wandirpereira.domain.entity.Cliente;
+import com.github.wandirpereira.domain.entity.ItemPedido;
 import com.github.wandirpereira.domain.entity.Pedido;
+import com.github.wandirpereira.domain.entity.Produto;
 import com.github.wandirpereira.domain.enums.StatusPedido;
+import com.github.wandirpereira.domain.repository.ClientesRepository;
+import com.github.wandirpereira.domain.repository.ItensPedidoRepository;
+import com.github.wandirpereira.domain.repository.PedidosRepository;
+import com.github.wandirpereira.domain.repository.ProdutosRepository;
+import com.github.wandirpereira.exception.RegraNegocioException;
+import com.github.wandirpereira.rest.dto.ItemPedidoDTO;
 import com.github.wandirpereira.rest.dto.PedidoDTO;
 import com.github.wandirpereira.service.IPedidoService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class PedidoService implements IPedidoService {
+
+    private final PedidosRepository pedidosRepository;
+    private final ClientesRepository clientesRepository;
+    private final ProdutosRepository produtosRepository;
+
+    private final ItensPedidoRepository itensPedidoRepository;
+
+//    public PedidoService(PedidosRepository pedidosRepository, ClientesRepository clientesRepository, ProdutosRepository produtosRepository) {
+//        this.pedidosRepository = pedidosRepository;
+//        this.clientesRepository = clientesRepository;
+//        this.produtosRepository = produtosRepository;
+//    }
+
     @Override
-    public Pedido salvar(PedidoDTO dto) {
-        return null;
+    @Transactional
+    public Pedido salvar(PedidoDTO dto){
+        Integer idCliente = dto.getCliente();
+        Cliente cliente = clientesRepository.findById(idCliente).orElseThrow(() -> new RegraNegocioException("Código de cliente inválido!"));
+        Pedido pedido = new Pedido();
+        pedido.setTotal((dto.getTotal()));
+        pedido.setDataPedido(LocalDate.now());
+        pedido.setCliente(cliente);
+        List<ItemPedido> itensPedido = converterItens(pedido, dto.getItens());
+        pedidosRepository.save(pedido);
+        itensPedidoRepository.saveAll(itensPedido);
+        pedido.setItens(itensPedido);
+        return pedido;
+    }
+
+    private List<ItemPedido> converterItens(Pedido pedido, List<ItemPedidoDTO> itens){
+        if(itens.isEmpty()){
+            throw new RegraNegocioException("Não é possível realizar um pedido sem itens!");
+        }
+        return itens
+                .stream()
+                .map( dto -> {
+                    Integer idProduto = dto.getProduto();
+                    Produto produto = produtosRepository
+                            .findById(idProduto)
+                            .orElseThrow(
+                                    () -> new RegraNegocioException("Código de produto inválido: " + idProduto)
+                            );
+                    ItemPedido itemPedido = new ItemPedido();
+                    itemPedido.setQuantidade(dto.getQuantidade());
+                    itemPedido.setPedido(pedido);
+                    itemPedido.setProduto(produto);
+                    return itemPedido;
+        }).collect(Collectors.toList());
     }
 
     @Override
